c36504c1b5c8b9cd5cf0378eaeeaa5c56c460b2c
Warrick Corfe-Tan <warrick@Warricks-MacBook-Pro.local>
2018-04-13 19:59:35 +1200

    changed directories

--- react-coin/src/index.js
+++ react-coin/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch  } from 'react-router-dom';
 import Header from './components/common/Header';
-import List from './components/common/list/List';
+import List from './components/list/List';
 import './index.css';
 
 const App = () => {
     return (
         <div>
             <Header />
-            
+
             <List />
         </div>
     );
